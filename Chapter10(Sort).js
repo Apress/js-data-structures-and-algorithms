4c5ba2656f9ad9ab081e4705150b75e7fb723710
Sammie Bae <baesammie@gmail.com>
2020-06-10 21:22:19 -0700

    Update Chapter10(Sort).js

--- Chapter10(Sort).js
+++ Chapter10(Sort).js
@@ -1,7 +1,6 @@
-
 function bubbleSort(array) {
     for (var i = 0, arrayLength = array.length; i < arrayLength; i++) {
-        for (var j = 0; j < arrayLength - 1 - i; j++) {
+        for (var j = 0; j <= i; j++) {
             if (array[j] > array[j+1]) {
                 swap(array, i, j);
             }
@@ -10,13 +9,14 @@ function bubbleSort(array) {
     return array;
 }
 
-bubbleSort([6, 1, 2, 3, 4, 5]); // [1,2,3,4,5,6]
-
 function swap(array, index1, index2) {
     var temp = array[index1];
     array[index1] = array[index2];
     array[index2] = temp;
 }
+
+bubbleSort([6, 1, 2, 3, 4, 5]); // [1,2,3,4,5,6]
+
 
 function selectionSort(items) {
     var len = items.length,
